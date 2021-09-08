5dbcb9b4a66e646cc227ff16b2e30ca71e5c9d2a
Anantha Krishnan <anantha_krishnan@outlook.com>
2021-09-08 12:14:28 +0530

    refactoring routes

--- natours/app.js
+++ natours/app.js
@@ -1,15 +1,16 @@
 const express = require('express');
-const fs = require('fs');
+
 //morgan gives us nice logging
 const morgan = require('morgan');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-//use middlewares
-// by default, express won't append body to req object. Hence middleware support is needed
-
 // 1) MIDDLEWARES
+//logging purpose
 app.use(morgan('dev'));
+// by default, express won't append body to req object. Hence middleware support is needed
 app.use(express.json());
 
 //creating your own middleware
@@ -25,129 +26,11 @@ app.use((req, res, next) => {
   next();
 });
 
-//synchronous code outside of callback is non-blocking, will goto thread pool
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8')
-);
-
-/* app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello from app', app: 'Natours' });
-});
-
-app.post('/', (req, res) => {
-  res.status(200).send('You can post data here');
-}); */
-
-// 2) Route Handlers
-const getAllTours = (req, res) => {
-  res.status(200).json(
-    //JSend response structure
-    {
-      status: 'success',
-      result: tours.length,
-      data: {
-        tours,
-      },
-    }
-  );
-};
-
-const getTour = (req, res) => {
-  //convert string to number
-  const id = +req.params.id;
-  const tour = tours.find((tour) => tour.id === id);
-  if (!tour) {
-    res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-  res.status(200).json({
-    status: 'success',
-    //appended from middleware
-    requestedAt: req.requestTime,
-    data: {
-      tour,
-    },
-  });
-};
-const createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-
-  const newTour = Object.assign({ id: newId }, req.body);
-  tours.push(newTour);
-  //use async version
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      //created
-      res.status(201).json(newTour);
-    }
-  );
-};
-
-const getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'route not implemented yet',
-  });
-};
-
-const createUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'route not implemented yet',
-  });
-};
-
-const getUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'route not implemented yet',
-  });
-};
-
-const updateUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'route not implemented yet',
-  });
-};
-
-const deleteUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'route not implemented yet',
-  });
-};
-
-/*
-app.get('/api/v1/tours', getAllTours);
-
-
-app.get('/api/v1/tours/:id', getTour);
-
-app.post('/api/v1/tours', createTour); */
-
-// 3) API Routes
-
-// creating multiple routers
-const tourRouter = express.Router();
-const userRouter = express.Router();
-
-// defining paths
-tourRouter.route('/').get(getAllTours).post(createTour);
-tourRouter.route('/:id').get(getTour);
-
-userRouter.route('/').get(getAllUsers).post(createUser);
-userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
-
 // mounting routers
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-// 4) Start server
+// Start server
 const port = 3000;
 app.listen(port, () => {
   console.log(`Listening at localhost:${port}`);
